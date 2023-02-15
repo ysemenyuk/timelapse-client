46d5e89ed724180553dd23840fc7262c2da82658
ysemenuk <invent@list.ru>
2023-02-15 09:24:49 +0300

    add react player

--- src/components/CameraLive/CameraLive.jsx
+++ src/components/CameraLive/CameraLive.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import { useOutletContext } from 'react-router-dom';
-// import { Player } from 'video-react';
+import ReactPlayer from 'react-player';
 
 function CameraLive() {
   const { selectedCamera } = useOutletContext();
@@ -9,13 +9,14 @@ function CameraLive() {
   return (
     <Col md={12} className="mb-4">
       <div>{`Name: ${selectedCamera.name}`}</div>
-      <video src="ws://localhost:9999" controls muted width="100%" height="100%">
+      {/* <video src="http://localhost:8000/live/s1.flv" controls muted width="100%" height="100%">
         <source type="video/mp4" />
-      </video>
-      {/* <Player
-        autoPlay
-        src="ws://localhost:9999"
-      /> */}
+      </video> */}
+      <ReactPlayer
+        url="http://localhost:8000/live/s1.flv"
+        muted
+        playing
+      />
     </Col>
   );
 }
